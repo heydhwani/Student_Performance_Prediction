fc3fe3c5250339695324cac55e9e63d2fdf67d00
Dhwani <dhwanijain731@gmail.com>
2025-12-07 18:41:39 +0530

    imported required libraries in train.py file

---
+++ src/train.py
@@ -0,0 +1,12 @@
+import joblib
+from pathlib import Path
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
