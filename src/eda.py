baa4508f8288a2c9a576895b59c96be32d9af072
Dhwani <dhwanijain731@gmail.com>
2025-12-07 16:05:41 +0530

    EXTRACTED NUMBER OF ROWS COLUMNS IN DATASET

--- src/eda.py
+++ src/eda.py
@@ -3,3 +3,11 @@ from pathlib import Path
 import seaborn as sns
 import matplotlib.pyplot as plt
 from data_utils import load_datasets
+
+def run_eda():
+    df = load_datasets("../data")
+    out_dir = Path("../reports/figures")
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    print("Data shape:", df.shape)
+    print(df.info())
